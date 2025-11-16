5e53892e18248c26bbff07a4c68262575675524c
Valerita <valeritamirandarosas05@gmail.com>
2025-11-16 00:30:29 +0000

    CUando quiero usar la app en linea en el dominio que me dieron Error 400

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import { initializeFirestore, persistentLocalCache, type Firestore, CACHE_SIZE_U
 // La clave de API es la que estaba configurada con los dominios correctos.
 const firebaseConfig = {
   apiKey: "AIzaSyCwE7XOU4bu901_m4h2pQyjhDbS51vF05k",
-  authDomain: "amplified-hull-476802-q0.firebaseapp.com",
+  authDomain: "amplified-hull-476802-q0.web.app",
   projectId: "amplified-hull-476802-q0",
   storageBucket: "amplified-hull-476802-q0.appspot.com",
   messagingSenderId: "1048372229577",
