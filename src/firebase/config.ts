1e869b703a5d37327c9fc2499927b192baa92085
Valerita <valeritamirandarosas05@gmail.com>
2025-11-22 03:44:58 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,8 @@
-// Este archivo se deja intencionadamente en blanco.
-// La configuración de Firebase será proporcionada por el entorno de App Hosting.
-export const firebaseConfig = undefined;
+export const firebaseConfig = {
+  "projectId": "amplified-hull-476802-q0",
+  "appId": "1:1048372229577:web:570303f90a2b52ae06a418",
+  "apiKey": "AIzaSyCwE7XOU4bu901_m4h2pQyjhDbS51vF05k",
+  "authDomain": "amplified-hull-476802-q0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1048372229577"
+};
